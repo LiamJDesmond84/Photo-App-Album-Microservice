9657a8019de9a153c4250ac8a20601d844364b8e
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-15 14:29:14 -0500

    Updating albums route

--- src/main/java/com/liam/photoappalbumservice/controllers/AlbumController.java
+++ src/main/java/com/liam/photoappalbumservice/controllers/AlbumController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liam.photoappalbumservice.models.AlbumEntity;
@@ -20,6 +21,7 @@ import com.liam.photoappalbumservice.services.AlbumService;
 import shared.AlbumDTO;
 
 @RestController
+@RequestMapping("/api/users/{id}/albums")
 public class AlbumController {
 	
 	@Autowired
@@ -27,11 +29,7 @@ public class AlbumController {
 	
     Logger logger = LoggerFactory.getLogger(this.getClass());
     
-    @GetMapping( 
-            produces = { 
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-            })
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<AlbumEntity> userAlbums(@PathVariable String id) {
 
         List<AlbumEntity> returnValue = new ArrayList<>();
